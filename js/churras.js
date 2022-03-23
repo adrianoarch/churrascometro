8b412e9b659a4d2256f086d810de7dd263fee047
Adriano de Oliveira <adriano.98@gmail.com>
2022-03-23 00:00:56 -0300

    Adicionando arquivos extras Bootstrap. Adicionando quadro do resultado e logica inicial no JS

---
+++ js/churras.js
@@ -0,0 +1,40 @@
+window.addEventListener("load", () => {
+  const formulario = document.querySelector("#formulario");
+  const quantidadeAdultos = document.querySelector("#adultos");
+  const quantidadeAdultosquenaobebem = document.querySelector(
+    "#adultosquenaobebem"
+  );
+  const quantidadeCriancas = document.querySelector("#criancas");
+  const duracaoChurrasco = document.querySelector("#duracao");
+  const botaoCalcular = document.querySelector("#buttoncalcular");
+  const erroformulariovazio = document.querySelector("#erroformulariovazio");
+  const resultado  = document.querySelector("#resultado")
+
+  const limparerroformulario = () => {
+    quantidadeAdultos.addEventListener("input", (e) => {
+      erroformulariovazio.style.display = "none";
+    });
+    duracaoChurrasco.addEventListener("input", (e) => {
+      erroformulariovazio.style.display = "none";
+    });
+  };
+
+  limparerroformulario();
+
+  formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // console.log(quantidadeAdultos.value);
+
+    if (!quantidadeAdultos.value || !duracaoChurrasco.value) {
+      erroformulariovazio.style.display = "block";
+    }
+
+    resultado.classList.add("active")
+
+    
+    quantidadeAdultos.value = "";
+    quantidadeAdultosquenaobebem.value = "";
+    quantidadeCriancas.value = "";
+    duracaoChurrasco.value = "";
+  });
+});
