59bd7c3170b3e86167eb039915033660bc002aec
Adriano de Oliveira <adriano.98@gmail.com>
2022-03-22 21:56:30 -0300

    Iniciando Projeto Churrascômetro, com JS e BootStrap.

---
+++ js/churras.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", () => {
+const quantidadeAdultos = document.querySelector("#adultos")
+const quantidadeAdultosquenaobebem = document.querySelector('#adultosquenaobebem')
+const quantidadeCriancas = document.querySelector('#criancas')
+const duracaoChurrasco = document.querySelector('#duracao')
+const botaoCalcular = document.querySelector('#buttoncalcular')
+
+botaoCalcular.addEventListener('submit', (e) => {
+    // e.preventDefault()
+    console.log("oi");
+})
+
+})
