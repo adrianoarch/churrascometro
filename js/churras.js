27702623149b866884c9ee64f69fb6d478da03c2
Adriano de Oliveira <adriano.98@gmail.com>
2022-03-24 15:10:51 -0300

    Configurando programação e criação dos resultados na tela

--- js/churras.js
+++ js/churras.js
@@ -1,5 +1,4 @@
 window.addEventListener("load", () => {
-
   const formulario = document.querySelector("#formulario");
   const quantidadeAdultos = document.querySelector("#adultos");
   const quantidadeAdultosquenaobebem = document.querySelector(
@@ -9,7 +8,7 @@ window.addEventListener("load", () => {
   const duracaoChurrasco = document.querySelector("#duracao");
   const botaoCalcular = document.querySelector("#buttoncalcular");
   const erroformulariovazio = document.querySelector("#erroformulariovazio");
-  const resultado  = document.querySelector("#resultado")
+  const resultado = document.querySelector("#resultado");
 
   const limparerroformulario = () => {
     quantidadeAdultos.addEventListener("input", (e) => {
@@ -28,15 +27,76 @@ window.addEventListener("load", () => {
 
     if (!quantidadeAdultos.value || !duracaoChurrasco.value) {
       erroformulariovazio.style.display = "block";
+      return;
     }
 
-    resultado.classList.add("active")
+    const adultos = quantidadeAdultos.value;
+    const adultossemalcool = quantidadeAdultosquenaobebem.value;
+    const criancas = quantidadeCriancas.value;
+    const duracao = duracaoChurrasco.value;
 
-    
+    const carnePorPessoa = (duracao) => {
+      return duracao >= 4 ? 650 : 400;
+    };
+
+    const cervejaPorPessoa = (duracao) => {
+      return duracao >= 4 ? 2000 : 1200;
+    };
+
+    const bebidaPorPessoa = (duracao) => {
+      return duracao >= 4 ? 1500 : 1000;
+    };
+
+    const quantidadeTotalCarne =
+      carnePorPessoa(duracao) * adultos +
+      (carnePorPessoa(duracao) / 2) * criancas;
+
+    const quantidadeTotalCerveja = cervejaPorPessoa(duracao) * adultos;
+
+    const quantidadeTotalBebida =
+      bebidaPorPessoa(duracao) * adultos +
+      (bebidaPorPessoa(duracao) / 2) * criancas;
+
+    console.log(quantidadeTotalCarne);
+    console.log(quantidadeTotalCerveja);
+    console.log(quantidadeTotalBebida);
+
+    const totalCarne = document.createElement("div");
+    totalCarne.classList.add("carne");
+    totalCarne.insertAdjacentHTML(
+      "beforeend",
+      `${quantidadeTotalCarne / 1000} Kg de carne`
+    );
+
+    const totalCerveja = document.createElement("div");
+    totalCerveja.classList.add("cerveja");
+    totalCerveja.insertAdjacentHTML(
+      "beforeend",
+      `${Math.ceil(quantidadeTotalCerveja / 350)} latas de cerveja`
+    );
+
+    const totalRefriAgua = document.createElement("div");
+    totalRefriAgua.classList.add("refriagua");
+    totalRefriAgua.insertAdjacentHTML(
+      "beforeend",
+      `${Math.ceil(quantidadeTotalBebida / 2000)} garrafas de refrigerante`
+    );
+
+    const botaoVoltar = document.createElement("div");
+    botaoVoltar.classList.add("btn", "btn-dark", "btn-lg", "mx-auto", "botaovoltar");
+    botaoVoltar.setAttribute("type", "submit");
+    botaoVoltar.insertAdjacentHTML("beforeend", "Voltar");
+
+    resultado.appendChild(totalCarne);
+    resultado.appendChild(totalCerveja);
+    resultado.appendChild(totalRefriAgua);
+    resultado.appendChild(botaoVoltar);
+
+    resultado.classList.add("active");
+
     quantidadeAdultos.value = "";
     quantidadeAdultosquenaobebem.value = "";
     quantidadeCriancas.value = "";
     duracaoChurrasco.value = "";
   });
 });
-
